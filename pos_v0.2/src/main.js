06c47f36bfe57f1386398ee84f373c4ebc73579a
meiMe7 <959383478@qq.com>
2015-02-06 18:00:22 +0800

    重构p_0.2,规范命名

--- pos_v0.2/src/main.js
+++ pos_v0.2/src/main.js
@@ -1,14 +1,16 @@
 //TODO: Please write code in this file.
 function printInventory(inputs) {
-    var collection_a = create_Product_barcode_list();
-    var collection_b = create_barcode(inputs);
-    var result = create_list(collection_a,collection_b);
-    print_shopping_list(result);
+    var collection_a = loadAllItems();
+    var collection_b = createBarcodeList(inputs);
+    var result = createShoppingList(collection_a,collection_b);
+    printShoppingList(result);
 }
 
-function create_list(collection_a,collection_b){
+function createShoppingList(collection_a,collection_b){
+    console.log(collection_a);
     var result = [];
-    for(var i = 0;i<collection_a.length;i++){
+
+    for(var object_i in collection_a){
         var obj = {
             barcode: '',
             name: '',
@@ -16,49 +18,28 @@ function create_list(collection_a,collection_b){
             price: 0,
             count_temp:0
         };
-        for(var j=0;j<collection_b.length;j++){
-
-            if(collection_a[i].barcode == collection_b[j].barcode){
-                obj.barcode = collection_a[i].barcode;
-                obj.name = collection_a[i].name;
-                obj.unit = collection_a[i].unit;
-                obj.price = collection_a[i].price;
-                obj.count_temp = collection_b[j].count_temp;
-
+        for(var object_j in collection_b){
+            if(collection_a[object_i].barcode == collection_b[object_j].barcode){
+                obj.barcode = collection_a[object_i].barcode;
+                obj.name = collection_a[object_i].name;
+                obj.unit = collection_a[object_i].unit;
+                obj.price = collection_a[object_i].price;
+                obj.count_temp = collection_b[object_j].count_temp;
+                result.push(obj);
             }
-        } result.push(obj);
+        }
     }
+
     return result;
 }
-function create_Product_barcode_list(){
-    var Product_barcode_list = [{
-        barcode: 'ITEM000000',
-        name: '可口可乐',
-        unit: '瓶',
-        price: 3.00
 
-    },
-        {
-            barcode: 'ITEM000001',
-            name: '雪碧',
-            unit: '瓶',
-            price: 3.00
-        },
-        {
-            barcode: 'ITEM000004',
-            name: '电池',
-            unit: '个',
-            price: 2.00
-        }];
-    return Product_barcode_list;
-}
-function create_barcode(collection_a){
+function createBarcodeList(collection_a){
     var result =[];
 
     for (var i=0;i< collection_a.length;i++) {
-        var  sum = 1;
+        var  sum = 0;
         var obj = {};
-        for (var j=i+1;j< collection_a.length;j++){
+        for (var j=0;j< collection_a.length;j++){
             if(collection_a[i] == collection_a[j]) {
                 sum ++;
                 i=j;
@@ -70,21 +51,23 @@ function create_barcode(collection_a){
         obj.count_temp = sum;
         result.push(obj);
     }
+
     return result;
 }
-function print_shopping_list(inputs) {
-    var str_header = '***<没钱赚商店>购物清单***'+'\n';
-    var str_foot_a = '----------------------'+'\n';
-    var str_foot_b = '**********************';
-    var str_body ='';
-    var all_count = 0;
+function printShoppingList(inputs) {
+    var stringHeader = '***<没钱赚商店>购物清单***'+'\n';
+    var stringFootA = '----------------------'+'\n';
+    var stringFootB = '**********************';
+    var stringBody ='';
+    var allCount = 0;
+
     for(var i in inputs){
         var count = parseFloat(inputs[i].price*inputs[i].count_temp).toFixed(2);
         var price = parseFloat(inputs[i].price).toFixed(2);
-        str_body = str_body+'名称：'+inputs[i].name+'，'+'数量：'+inputs[i].count_temp+inputs[i].unit+'，'+'单价：'+price+'(元)，小计：'+count+'(元)\n';
-        all_count = all_count+inputs[i].price*inputs[i].count_temp;
+        stringBody = stringBody+'名称：'+inputs[i].name+'，'+'数量：'+inputs[i].count_temp+inputs[i].unit+'，'+'单价：'+price+'(元)，小计：'+count+'(元)\n';
+        allCount = allCount+inputs[i].price*inputs[i].count_temp;
     }
-    var str_count = '总计：'+parseFloat(all_count).toFixed(2)+'(元)\n';
-    var str_all = str_header+str_body+str_foot_a+str_count+str_foot_b;
-    console.log(str_all);
+    var stringCount = '总计：'+parseFloat(allCount).toFixed(2)+'(元)\n';
+    var stringAll = stringHeader+stringBody+stringFootA+stringCount+stringFootB;
+    console.log(stringAll);
 }
