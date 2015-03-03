5319fbb78cc4ae270cae56cd8b331d7dfd3ce4c9
meiMe7 <959383478@qq.com>
2015-03-03 17:18:50 +0800

    规范pos_v2_3命名

--- pos_v2_3rdlib/src/main.js
+++ pos_v2_3rdlib/src/main.js
@@ -1,14 +1,14 @@
 //TODO: Please write code in this file.
 function printInventory(inputs) {
-    var turn_barcode = new Turned_barcode_to_list();
-    turn_barcode.collection = inputs;
-    turn_barcode.split_barcode();//按照特殊分隔符统计条形码的数量
-    var weight = turn_barcode.weight();//将条形码中计数单位为"斤"的商品选择出来组成单独数组,采用称重的方式得到称重商品条形码重量对象数组
-    turn_barcode.count_barcode();//统计除称重商品意外的普通商品相同条形码的数量
-    turn_barcode.all_goods(weight);//合并称重商品和普通商品
-    turn_barcode.collection_message = loadAllItems();//引入商品信息
-    turn_barcode.create_list();//建立购买商品对象与商品信息对应的购物信息数组
-    turn_barcode.sail_collection_message = loadPromotions();//引入减价商品信息
-    print_shopping_list(turn_barcode.collection, turn_barcode.sum_count_price());//将购物信息和总计价格输出打印
+    var turnBarcode = new TurnedBarcodeToList();
+    turnBarcode.collection = inputs;
+    turnBarcode.SplitBarcode();//按照特殊分隔符统计条形码的数量
+    var weight = turnBarcode.Weight();//将条形码中计数单位为"斤"的商品选择出来组成单独数组,采用称重的方式得到称重商品条形码重量对象数组
+    turnBarcode.CountBarcode();//统计除称重商品意外的普通商品相同条形码的数量
+    turnBarcode.AllGoods(weight);//合并称重商品和普通商品
+    turnBarcode.collectionMessage = loadAllItems();//引入商品信息
+    turnBarcode.CreateList();//建立购买商品对象与商品信息对应的购物信息数组
+    turnBarcode.sailCollectionMessage = loadPromotions();//引入减价商品信息
+    PrintShoppingList(turnBarcode.collection, turnBarcode.SumCountPrice());//将购物信息和总计价格输出打印
 
 }
--- pos_v2_3rdlib/src/turned_barcode_to_list.js
+++ pos_v2_3rdlib/src/turned_barcode_to_list.js
@@ -2,32 +2,32 @@
  * Created by csc on 15-2-3.
  */
 //条形码转换成购买商品的信息
-function Turned_barcode_to_list() {
+function TurnedBarcodeToList() {
     var collection = [];
-    var collection_message = [];
-    var sail_collection_message = [];
+    var collectionMessage = [];
+    var sailCollectionMessage = [];
 }
 //将条形码，按照条形码号统计出数量，返回条形码对象数组［｛barcode:'',count:''｝］
-Turned_barcode_to_list.prototype.count_barcode = function () {
-    var collection_a = this.collection;
+TurnedBarcodeToList.prototype.CountBarcode = function () {
+    var collectionA = this.collection;
     var result = [];
-    for (var i = 0; i < collection_a.length; i++) {
+    for (var i = 0; i < collectionA.length; i++) {
         var sum = 1;
         var obj = {};
-        for (var j = i + 1; j < collection_a.length; j++) {
-            if (collection_a[i] == collection_a[j]) {
+        for (var j = i + 1; j < collectionA.length; j++) {
+            if (collectionA[i] == collectionA[j]) {
                 sum++;
                 i = j;
             }
         }
-        obj = {barcode: collection_a[i], count_temp: sum};
+        obj = {barcode: collectionA[i], count_temp: sum};
         obj.count_temp = sum;
         result.push(obj);
     }
     this.collection = result;
 };
 //将条形码，按照条形码号特殊符号＂－＂统计出数量，返回条形码对象数组［｛barcode:'',count:''｝］
-Turned_barcode_to_list.prototype.split_barcode = function () {
+TurnedBarcodeToList.prototype.SplitBarcode = function () {
     var collection = this.collection;
     for (var i = 0; i < collection.length; i++) {
         var arr = (collection[i]).split("-");
@@ -42,61 +42,60 @@ Turned_barcode_to_list.prototype.split_barcode = function () {
     this.collection = collection;
 };
 //将条形码中计数单位为"斤"的商品选择出来组成单独数组,采用称重的方式得到称重商品条形码重量对象数组result[{barcode:,count_temp:}],turn_barcode.collection中不包括称重商品的条形码
-Turned_barcode_to_list.prototype.weight = function () {
-    var collection_a = this.collection;
+TurnedBarcodeToList.prototype.Weight = function () {
+    var collectionA = this.collection;
     var result = [];
-    for (var i in collection_a) {
-        var collection_b = loadAllItems();
-        for (var j in collection_b) {
-            if (collection_a[i] == collection_b[j].barcode && collection_b[j].unit == "斤") {
-                result.push(collection_a[i]);
+    for (var i in collectionA) {
+        var collectionB = loadAllItems();
+        for (var j in collectionB) {
+            if (collectionA[i] == collectionB[j].barcode && collectionB[j].unit == "斤") {
+                result.push(collectionA[i]);
             }
         }
     }
-    for (var i in collection_a) {
-        var collection_b = loadAllItems();
+    for (var i in collectionA) {
+        var collectionC = loadAllItems();
         for (var j in result) {
-            if (collection_a[i] == result[j]) {
-                collection_a[i] = collection_a[++i];
+            if (collectionA[i] == result[j]) {
+                collectionA[i] = collectionA[++i];
                 --i;
-                collection_a.length = collection_a.length - 1;
+                collectionA.length = collectionA.length - 1;
             }
         }
     }
-    this.collection = collection_a;
-    var collection_a = result;
-    var result_b = [];
-    for (var i = 0; i < collection_a.length; i++) {
+    this.collection = collectionA;
+    var collectionD = result;
+    var resultB = [];
+    for (var i = 0; i < collectionD.length; i++) {
         var sum = 1;
         var obj = {};
-        for (var j = i + 1; j < collection_a.length; j++) {
-            if (collection_a[i] == collection_a[j]) {
+        for (var j = i + 1; j < collectionD.length; j++) {
+            if (collectionD[i] == collectionD[j]) {
                 sum++;
                 i = j;
             }
         }
-        obj = {barcode: collection_a[i], count_temp: sum};
+        obj = {barcode: collectionD[i], count_temp: sum};
         obj.count_temp = sum;
-        result_b.push(obj);
+        resultB.push(obj);
     }
-    return result_b;
+    return resultB;
 };
 //称重商品与普通商品合并得到商品数量对象数组[{barcode:,count_temp:}];
-Turned_barcode_to_list.prototype.all_goods = function (weight) {
+TurnedBarcodeToList.prototype.AllGoods = function (weight) {
 
-    var collection_a = this.collection;
-    var collection_b = weight;
-    for (var i in weight) {
-        collection_a.push(weight[i]);
+    var collectionA = this.collection;
+      for (var i in weight) {
+        collectionA.push(weight[i]);
     }
-    this.collection = collection_a;
+    this.collection = collectionA;
 };
 //按照条形码对象数组,条形码与商品信息相对应，得到购买商品信息对象数组
-Turned_barcode_to_list.prototype.create_list = function () {
-    var collection_a = this.collection_message;
-    var collection_b = this.collection;
+TurnedBarcodeToList.prototype.CreateList = function () {
+    var collectionA = this.collectionMessage;
+    var collectionB = this.collection;
     var result = [];
-    for (var i = 0; i < collection_a.length; i++) {
+    for (var i = 0; i < collectionA.length; i++) {
         var obj = {
             barcode: '',
             name: '',
@@ -104,13 +103,13 @@ Turned_barcode_to_list.prototype.create_list = function () {
             price: 0,
             count_temp: 0
         };
-        for (var j = 0; j < collection_b.length; j++) {
-            if (collection_a[i].barcode == collection_b[j].barcode) {
-                obj.barcode = collection_a[i].barcode;
-                obj.name = collection_a[i].name;
-                obj.unit = collection_a[i].unit;
-                obj.price = collection_a[i].price;
-                obj.count_temp = collection_b[j].count_temp;
+        for (var j = 0; j < collectionB.length; j++) {
+            if (collectionA[i].barcode == collectionB[j].barcode) {
+                obj.barcode = collectionA[i].barcode;
+                obj.name = collectionA[i].name;
+                obj.unit = collectionA[i].unit;
+                obj.price = collectionA[i].price;
+                obj.count_temp = collectionB[j].count_temp;
             }
         }
         if (obj.barcode != '')result.push(obj);
@@ -118,22 +117,22 @@ Turned_barcode_to_list.prototype.create_list = function () {
     this.collection = result;
 };
 //按照商品信息，和买一送一商品信息条形码相等，得到购买商品总价格即：总计，和购买商品中赠送的商品的总价格即：节省
-Turned_barcode_to_list.prototype.sum_count_price = function () {
-    var collection_a = this.collection;
-    var collection_b = this.sail_collection_message;
-    var all_count = 0;
-    var sail_count = 0.00;
+TurnedBarcodeToList.prototype.SumCountPrice = function () {
+    var collectionA = this.collection;
+    var collectionB = this.sail_collection_message;
+    var allCount = 0;
+    var sailCount = 0.00;
 
-    for (var i in collection_a) {
-        all_count = all_count + collection_a[i].price * collection_a[i].count_temp;
-        for (var k in  collection_b) {
-            for (var j in collection_b[k].barcode) {
-                if (collection_b[k].barcode[j] == collection_a[i].barcode) {
-                    sail_count = sail_count + collection_a[i].price;
+    for (var i in collectionA) {
+        allCount = allCount + collectionA[i].price * collectionA[i].count_temp;
+        for (var k in  collectionB) {
+            for (var j in collectionB[k].barcode) {
+                if (collectionB[k].barcode[j] == collectionA[i].barcode) {
+                    sailCount = sailCount + collectionA[i].price;
                 }
             }
         }
     }
-    all_count = parseFloat(all_count - sail_count).toFixed(2);
-    return [all_count, sail_count];
+    allCount = parseFloat(allCount - sailCount).toFixed(2);
+    return [allCount, sailCount];
 };
