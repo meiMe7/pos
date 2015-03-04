316b69d84b021ccaef4aa5b1ce6bf922de75e639
meiMe7 <959383478@qq.com>
2015-03-04 10:36:07 +0800

    将pos_v0部分for替换为foreach

--- pos_v0/src/main.js
+++ pos_v0/src/main.js
@@ -6,17 +6,14 @@ function printInventory(inputs) {
     var stringBody = '';
     var allCount = 0;
 
-    for (var object in inputs) {
-        var count = parseFloat(inputs[object].price * inputs[object].count).toFixed(2);
-        var price = parseFloat(inputs[object].price).toFixed(2);
-        stringBody = stringBody + '名称：' + inputs[object].name + '，' + '数量：' + inputs[object].count + inputs[object].unit + '，' + '单价：' + price + '(元)，小计：' + count + '(元)\n';
-        allCount = allCount + inputs[object].price * inputs[object].count;
-    }
+    inputs.forEach(function (object) {
+        var count = parseFloat(object.price * object.count).toFixed(2);
+        var price = parseFloat(object.price).toFixed(2);
+        stringBody = stringBody + '名称：' + object.name + '，' + '数量：' + object.count + object.unit + '，' + '单价：' + price + '(元)，小计：' + count + '(元)\n';
+        allCount = allCount + object.price * object.count;
+    });
 
     var stringCount = '总计：' + parseFloat(allCount).toFixed(2) + '(元)\n';
-    var stringAll = stringHeader + stri
-
-
-    ngBody + stringFootA + stringCount + stringFootB;
+    var stringAll = stringHeader + stringBody + stringFootA + stringCount + stringFootB;
     console.log(stringAll);
 }
