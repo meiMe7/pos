258463cc483326dffd16f6ae6437082cfbb87eb5
meiMe7 <959383478@qq.com>
2015-03-14 09:38:17 +0800

    underscore重构pos_v1

--- pos_v0/src/main.js
+++ pos_v0/src/main.js
@@ -5,13 +5,12 @@ function printInventory(inputs) {
     var stringFootB = '**********************';
     var stringBody = '';
     var allCount = 0;
-
-    inputs.forEach(function (object) {
+    _.filter(inputs,function(object){
         var count = parseFloat(object.price * object.count).toFixed(2);
         var price = parseFloat(object.price).toFixed(2);
         stringBody = stringBody + '名称：' + object.name + '，' + '数量：' + object.count + object.unit + '，' + '单价：' + price + '(元)，小计：' + count + '(元)\n';
-        allCount = allCount + object.price * object.count;
-    });
+        allCount = allCount + object.price * object.count;});
+
 
     var stringCount = '总计：' + parseFloat(allCount).toFixed(2) + '(元)\n';
     var stringAll = stringHeader + stringBody + stringFootA + stringCount + stringFootB;
--- pos_v2_3rdlib/src/main.js
+++ pos_v2_3rdlib/src/main.js
@@ -1,5 +1,5 @@
 //TODO: Please write code in this file.
 function printInventory(inputs) {
-    var turnBarcode = BarcodeScanner(inputs);//将商品信息用条形码扫描仪扫描
+    var turnBarcode = new BarcodeScanner(inputs);//将商品信息用条形码扫描仪扫描
     PrintShoppingList(turnBarcode.collection, turnBarcode.SumCountPrice());//将购物信息和总计价格输出打印
 }
